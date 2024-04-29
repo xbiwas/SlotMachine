8239dda2086a87bf8fa431a2129aeaaef38e6aa7
xbiwas <biwas2059@gmail.com>
2024-04-29 14:35:58 +0545

    Functions for depositing, number of lines and betting

---
+++ main.py
@@ -0,0 +1,55 @@
+MAX_LINES = 3
+MIN_BET = 1
+MAX_BET = 100
+
+def deposit():
+    while True:
+        amount = input("Enter the amount you want to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                print("Deposited")
+                break
+            else:
+                print("Amount should be greater than zero.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+def numberOfLines():
+    while True:
+        lines = input("Enter the number of lines. ")
+        if lines.isdigit():
+            lines = int(lines)
+        
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Lines should be between 1-" + str(MAX_LINES))
+        
+        else:
+            print("Please enter a number")
+    return lines
+
+def getBets():
+    while True:
+        bet = input("Enter the betting amount. $")
+        if bet.isdigit():
+            bet = int(bet)
+        
+            if MIN_BET <= bet <= MAX_BET:
+                break
+            else:
+                print(f"Betting amount should be between {MAX_BET} - {MIN_BET} ")
+        
+        else:
+            print("Please enter a number")
+    return bet
+
+def main():    
+    balance = deposit()
+    lines = numberOfLines()
+    bets = getBets()
+    totalBet = lines * bets
+    print(f"You are betting ${bets} on {lines} lines. Your total betting is ${totalBet}.")
+main()
